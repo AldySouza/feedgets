dfcac23c75152aa043dc493471a9106ea812902f
Aldy Souza <60198926+AldySouza@users.noreply.github.com>
2022-06-13 11:43:58 -0300

    feat/add port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3334, () => {
+app.listen(process.env.PORT || 3334, () => {
   console.log('HTTP server running on port 3334!');  
 })
